70bf5c9af8863e92bddd5f4f1705da71f1e009f4
Erwin Medina <erwin.l.medina@gmail.com>
2021-05-10 11:37:04 -0700

    mypets form css implemented. still working on functionality

--- src/pages/MyPets/MyPets.jsx
+++ src/pages/MyPets/MyPets.jsx
@@ -1,39 +1,42 @@
+import "./MyPets.css"
+
 export default function MyPets() {
     return (
         <div className="container">
-             <div class="container mt-5">
-                <div class="card">
+             <div className="container cardContainer mt-5">
+                <div className="card cardCardContainer p-3">
+                    <h3>Adoption Form</h3>
                     <div className="form-group">
                         <label htmlFor="">Pet Name:</label>
-                        <input type="text"/>
+                        <input className="form-control" type="text"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="">Pet Age:</label>
-                        <input type="text"/>
+                        <input className="form-control" type="number"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="">Pet Gender:</label>
-                        <input type="text"/>
+                        <input className="form-control" placeholder="M or F" type="text"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="">Pet Breed 1:</label>
-                        <input type="text"/>
+                        <input className="form-control" type="text"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="">Pet Breed 2:</label>
-                        <input type="text"/>
+                        <input className="form-control" type="text"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="">Pet Color:</label>
-                        <input type="text"/>
+                        <input className="form-control" type="text"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="">Email:</label>
-                        <input type="text"/>
+                        <input className="form-control" type="text"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="">Phone #:</label>
-                        <input type="text"/>
+                        <input className="form-control" type="text"/>
                     </div>
                     <div className="btn btn-primary">Submit</div>
                 </div>
