940c12e9175d06d8145faaf85eee75a43c0a77cd
Erwin Medina <erwin.l.medina@gmail.com>
2021-05-16 18:54:44 -0700

    implemented ensureLogIn on pets route

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const petsCtrl = require('../../controllers/api/pets');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 router.get("/", petsCtrl.getAll);
-router.post("/", petsCtrl.petCreate);
-router.delete("/:id", petsCtrl.petDelete);
-router.put("/:id", petsCtrl.petUpdate);
+router.post("/", ensureLoggedIn, petsCtrl.petCreate);
+router.delete("/:id", ensureLoggedIn, petsCtrl.petDelete);
+router.put("/:id", ensureLoggedIn, petsCtrl.petUpdate);
 
 module.exports = router;
