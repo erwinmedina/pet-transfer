213eb29ea7895dbb025e95a6af6f33f3b3bff152
Erwin Medina <erwin.l.medina@gmail.com>
2021-07-18 22:50:06 -0700

    continued adjusting detail page, css

--- src/pages/AvailablePetsDetailsPage/AvailablePetsDetailsPage.jsx
+++ src/pages/AvailablePetsDetailsPage/AvailablePetsDetailsPage.jsx
@@ -4,6 +4,15 @@ import { useEffect, useState } from "react";
 import "./AvailablePetsDetailsPage.css"
 
 export default function AvailablePetsDetailsPage({pet}) {
+    const [message, setMessage] = useState(false);
+
+    function handleMessage(event) {
+        setMessage(true);
+    }
+    function discardSend(event) {
+        setMessage(false);
+    }
+
     function lowerUpperCase(string) {
         string = string.toLowerCase();
         string = string[0].toUpperCase() + string.slice(1);
@@ -12,78 +21,102 @@ export default function AvailablePetsDetailsPage({pet}) {
 
     return (
         <div className="AvailablePetsDetailsPage">
-
+            <h2 className="homepage-title">{pet.name}'s Detail Page!</h2>
+            <hr/>
+            <div className={`DetailsPageContainer ${message ? 'disablePage' : ''}`}>
+                <div className="leftHand">
+                    <div className="leftHandImage">
+                        <img className="" src={pet.sourceURL} alt=""/>
+                    </div>
+                    <div className="leftHandDetails">
+                        <span className="leftHandDetailsName">{lowerUpperCase(pet.name)}</span>
+                        <div className="leftHandDetailsColumns">
+                            <div className="columnLeftPill">
+                                <p>Type: </p>
+                                <p>{lowerUpperCase(pet.species)}</p>
+                            </div>
+                            <div className="columnLeftPill">
+                                <p>Breed: </p>
+                                <p>{lowerUpperCase(pet.breed)}</p>
+                            </div>
+                            <div className="columnLeftPill">
+                                <p>Age: </p>
+                                <p>{lowerUpperCase(pet.age)} yr(s) old</p>
+                            </div>
+                            <div className="columnLeftPill">
+                                <p>Sex: </p>
+                                <p>{lowerUpperCase(pet.gender)}</p>
+                            </div>
+                            <div className="columnLeftPill">
+                                <p>Color: </p>
+                                <p>{lowerUpperCase(pet.color)}</p>
+                            </div>
+                            <div className="columnLeftPill">
+                                <p>Details: </p>
+                                <p>{pet.additional}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                            
+                {/* Map/Location and Contact Owner Info */}
+                <div className="rightHand">
+                    <div className="rightHandMap">
+                        <img className="" src="https://www.coolbusinessideas.com/wp-content/uploads/2018/10/Google-Map-Now-Tells-You-Offers-From-Your-Favourite-Places-.jpg" alt=""/>
+                    </div>
+                    <div className="rightHandOwner">
+                        <p>Contact the Owner</p>
+                        <div className="columnLeftPill">
+                            <p>Username: </p>
+                            <p>{pet.user.name}</p>
+                        </div>
+                        <div className="columnLeftPill">
+                            <p>Email: </p>
+                            <p>{pet.user.email}</p>
+                        </div>
+                        <div className="columnLeftPill">
+                            <p>Phone #: </p>
+                            <p>{pet.phone}</p>
+                        </div>
+                        <div className="buttonContainer">
+                            <div onClick={() => handleMessage()} className="btn btn-primary">
+                                <i class="far fa-comment"></i> 
+                                Message
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             {/* Image and Animal Details */}
-            <div className="leftHand">
-                <div className="leftHandImage">
-                    <img className="" src={pet.sourceURL} alt=""/>
+            
+            { message &&
+                <div className="messageContainer">
+                    <div className="message">
+                        <h2>Message Owner</h2>
+                        <div className="subjectLine">
+                            <label >Subject:</label>
+                            <input placeholder={`${pet.name} inquiries..`}type="text" />
+                        </div>
+                        <br />
+                        <div className="messageBox">
+                            <label>Message:</label>
+                            <textarea name="" id="" cols="30" rows="4"></textarea>
+                        </div>
+                        <div className="messageButtonContainer">
+                            <div onClick={discardSend} className="btn btn-danger">
+                                <i class="fas fa-trash"></i>
+                                Discard
+                            </div>
+                            <div onClick={discardSend} className="btn btn-success">
+                                <i class="far fa-paper-plane"></i>
+                                Send
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
-                <div className="leftHandDetails">
-                    <span className="leftHandDetailsName">{lowerUpperCase(pet.name)}</span>
-                    <div className="leftHandDetailsColumns">
-                        <div className="columnLeftPill">
-                            <p>Type: </p>
-                            <p>{lowerUpperCase(pet.species)}</p>
-                        </div>
-                        <div className="columnLeftPill">
-                            <p>Breed: </p>
-                            <p>{lowerUpperCase(pet.breed)}</p>
-                        </div>
-                        <div className="columnLeftPill">
-                            <p>Age: </p>
-                            <p>{lowerUpperCase(pet.age)} yr(s) old</p>
-                        </div>
-                        <div className="columnLeftPill">
-                            <p>Sex: </p>
-                            <p>{lowerUpperCase(pet.gender)}</p>
-                        </div>
-                        <div className="columnLeftPill">
-                            <p>Color: </p>
-                            <p>{lowerUpperCase(pet.color)}</p>
-                        </div>
-                        <div className="columnLeftPill">
-                            <p>Details: </p>
-                            <p>{pet.additional}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                        
-            {/* Map/Location and Contact Owner Info */}
-            <div className="rightHand">
-                <div className="rightHandMap">
-                    <img className="" src="https://www.coolbusinessideas.com/wp-content/uploads/2018/10/Google-Map-Now-Tells-You-Offers-From-Your-Favourite-Places-.jpg" alt=""/>
-                </div>
-                <div className="rightHandOwner">
-                    <p>Contact the Owner</p>
-                    <div className="columnLeftPill">
-                        <p>Username: </p>
-                        <p>{pet.user.name}</p>
-                    </div>
-                    <div className="columnLeftPill">
-                        <p>Email: </p>
-                        <p>{pet.user.email}</p>
-                    </div>
-                    <div className="columnLeftPill">
-                        <p>Phone #: </p>
-                        <p>{pet.phone}</p>
-                    </div>
-                    <div className="buttonContainer">
-                        <div className="btn btn-primary">
-                            <i class="far fa-comment"></i> 
-                            Message
-                        </div>
-                        <div className="btn btn-primary">
-                            <i class="fas fa-envelope-open-text"></i>
-                            Email
-                        </div>
-                        <div className="btn btn-primary">
-                            <i class="fas fa-phone"></i>
-                            Call
-                        </div>
-                    </div>
-                </div>
-            </div>
+            }
+
 
         </div>
     )
